c2f8e6012bf75acef34feb1600b452451e81bee2
Eliseu Videira <eliseuvideira1@gmail.com>
2022-05-01 11:00:39 -0300

    :wrench: using input and output variables

--- chapter-02/01-simple-server/main.tf
+++ chapter-02/01-simple-server/main.tf
@@ -2,12 +2,18 @@ provider "aws" {
   region = "us-east-2"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
 resource "aws_security_group" "terraform_instance_sg" {
   name = "terraform-instance-security-group"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -21,10 +27,15 @@ resource "aws_instance" "terraform_instance" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World!" >index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
   tags = {
     Name = "terraform-instance"
   }
+}
+
+output "public_ip" {
+  value       = aws_instance.terraform_instance.public_ip
+  description = "The public ip address of the web server"
 }
